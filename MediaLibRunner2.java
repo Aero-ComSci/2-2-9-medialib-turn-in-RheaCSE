3a4c123edea979ed26cb25ce5df6839a49524118
RheaCSE <157422210+RheaCSE@users.noreply.github.com>
2026-02-10 22:20:36 -0800

    Delete MediaLibRunner2.java

--- MediaLibRunner2.java
+++
@@ -1,9 +0,0 @@
-public class MediaLibRunner2
-{
-public static void main(String[] args)
-{
-  System.out.println(MediaLib.getNumEntries());
-  MediaLib m1 = new MediaLib();
-  System.out.println(m1);
-}
-}
